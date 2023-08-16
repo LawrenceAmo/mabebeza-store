84c20ccc347bd6bd4e3b7ba493e27186371b6d5c
Lawrence Amo <LawrenceAmo@users.noreply.github.com>
2023-08-16 09:58:01 +0200

    fix approved_by by adding migrations

---
+++ database/migrations/2023_08_16_094837_add_approvedby_to_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->bigInteger('approved_by')->nullable();   
+            $table->bigInteger('updated_by')->nullable();
+             $table->date('approved_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropColumn('approved_by');
+            $table->dropColumn('updated_by');
+            $table->dropColumn('approved_at');
+
+        });
+    }
+};
