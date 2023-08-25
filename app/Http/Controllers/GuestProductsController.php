1a5b6307feacb1c83b2fe9ad11ee6564c1e0725f
Lawrence Amo <LawrenceAmo@users.noreply.github.com>
2023-08-25 10:48:32 +0200

    update product search

--- app/Http/Controllers/GuestProductsController.php
+++ app/Http/Controllers/GuestProductsController.php
@@ -189,21 +189,22 @@ class GuestProductsController extends Controller
         $keywords = explode('-', strtolower($name));
 
         $products = DB::table('products')
-                ->leftJoin('store_inventories', 'store_inventories.productID', '=', 'products.productID' )
-                ->leftJoin('product_photos', function ($join) {
-                    $join->on('product_photos.productID', '=', 'products.productID')
-                        ->whereRaw('product_photos.product_photoID = (SELECT MIN(product_photoID) FROM product_photos WHERE productID = products.productID)');
-                })
-                ->select('products.productID', 'products.name as product_name' , 'products.publish', 'products.availability', 'products.sku', 'products.cost_price', 'products.price', 'products.sale_price', 'product_photos.url', 'product_photos.title', 'products.type', 'store_inventories.quantity' )
-                ->where(function ($query) use ($keywords) {
-                    foreach ($keywords as $keyword) {
-                        $query->orWhereRaw('LOWER(name) like ?', ['%' . strtolower($keyword) . '%']);
-                    }
-                })
-                ->where( 'products.availability', '=',  true)
-                ->where( 'products.publish', '=', true)
-                ->groupBy('products.productID', 'products.name' , 'products.publish','product_photos.url', 'product_photos.title', 'products.availability', 'products.sku', 'products.cost_price','products.sale_price', 'products.price', 'products.type', 'store_inventories.quantity')
-                ->get();
+            ->leftJoin('store_inventories', 'store_inventories.productID', '=', 'products.productID')
+            ->leftJoin('product_photos', function ($join) {
+                $join->on('product_photos.productID', '=', 'products.productID')
+                    ->whereRaw('product_photos.product_photoID = (SELECT MIN(product_photoID) FROM product_photos WHERE productID = products.productID)');
+            })
+            ->select('products.productID', 'products.name as product_name', 'products.publish', 'products.availability', 'products.sku', 'products.cost_price', 'products.price', 'products.sale_price', 'product_photos.url', 'product_photos.title', 'products.type', 'store_inventories.quantity')
+            ->where(function ($query) use ($keywords) {
+                foreach ($keywords as $keyword) {
+                    $query->whereRaw('LOWER(name) like ?', ['%' . strtolower($keyword) . '%']);
+                }
+            })
+            ->where('products.availability', '=', true)
+            ->where('products.publish', '=', true)
+            ->groupBy('products.productID', 'products.name', 'products.publish', 'product_photos.url', 'product_photos.title', 'products.availability', 'products.sku', 'products.cost_price', 'products.sale_price', 'products.price', 'products.type', 'store_inventories.quantity')
+            ->get();
+
 
         // return $products;
         return view('pages.products.product_search')->with('products', $products);
