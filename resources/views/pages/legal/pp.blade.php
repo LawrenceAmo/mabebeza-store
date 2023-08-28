7f239e03ab209ca38679abdb1c3cd66034c076f6
Lawrence Amo <LawrenceAmo@users.noreply.github.com>
2023-08-28 17:17:07 +0200

    init

--- resources/views/pages/legal/pp.blade.php
+++ resources/views/pages/legal/pp.blade.php
@@ -4,32 +4,32 @@
     <main   class="m-0 p-0">
  
       <div class=" p-5 m-lg-5 mt-3 "> 
- 
+
           <div class="container">
               <h1>Privacy Policy</h1>
-              
+ 
               <p>Last Updated: 25 August 2023</p>
-      
+
               <p>Welcome to Mabebeza Retailers ("we", "us", or "our"). We respect your privacy and are committed to protecting your personal information. This Privacy Policy outlines how we collect, use, and safeguard your data when you use our website <a href="https://mabebeza.com">Mabebeza</a> (the "Site").</p>
-      
+
               <h2>Information We Collect</h2>
               <p>We may collect personal information such as your name, email address, postal address, phone number, and other data you provide when you use our Site, sign up for an account, make a purchase, or communicate with us.</p>
-      
+
               <h2>How We Use Your Information</h2>
               <p>We use the collected information to provide, maintain, and improve our services, process orders, communicate with you, and personalize your experience. We may also use your data for analytics and marketing purposes.</p>
-      
+
               <h2>Sharing Your Information</h2>
               <p>We do not sell, trade, or rent your personal information to third parties. However, we may share your data with trusted service providers who assist us in operating our business and servicing you.</p>
-      
+
               <h2>Cookies and Tracking</h2>
               <p>We use cookies and similar technologies to enhance your experience, analyze usage patterns, and improve our services. You can control cookies through your browser settings.</p>
-      
+
               <h2>Your Choices</h2>
               <p>You can update, correct, or delete your personal information by contacting us. You can also opt out of receiving marketing communications. However, essential service-related communications may still be sent.</p>
-      
+
               <h2>Security</h2>
               <p>We implement industry-standard security measures to protect your data. However, no method of transmission over the internet is completely secure, and we cannot guarantee absolute security.</p>
-      
+
               <h2>Changes to Privacy Policy</h2>
               <p>We may update our Privacy Policy from time to time. Any changes will be posted on this page, and the updated version will be effective as of the date of posting.</p>
       
